afa5ba4b44e9bd0453a5832556e816d45666db32
Adolfo Arnold <adolfo.arnold@gmail.com>
2017-06-22 10:46:38 +0200

    startapp

---
+++ XamarinWebViewApp/XamarinWebViewApp/XamarinWebViewApp/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinWebViewApp.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
